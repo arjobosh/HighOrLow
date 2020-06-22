0e49fd2c1c474c434e46fdf27c38d4133f15b4b0
Joshua Arber <arber@unlv.nevada.edu>
2020-06-21 21:42:57 -0700

    added card sprites

--- HighOrLow/Assets/HighOrLow.cs
+++ HighOrLow/Assets/HighOrLow.cs
@@ -1,46 +1,127 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Xml.Linq;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class HighOrLow : MonoBehaviour
 {
-    Deck deck;
-    List<Card> hand;
+    private Deck deck;
+    private List<Card> hand;
+    private List<GameObject> sprites;
+
     private int cardsToDraw;
     public Text deckText;
     public Text handText;
 
-    // Start is called before the first frame update
     void Start()
     {
         cardsToDraw = 2;
         deck = new Deck();
+        hand = new List<Card>();
+        sprites = new List<GameObject>();
     }
 
-    public void JudgeHand()
+    private string GenerateAssetPath(int value, string suit, string name, bool isFace)
     {
+        string path = "Assets/CardArt/" + suit + "/";
 
+        path += (value < 10) ? "0" : "";
+        path += value.ToString() + "_";
+        path += (isFace) ? name[0].ToString() : value.ToString();
+        path += "_" + suit[0].ToString();
+        path += ".png";
+
+        return path;
     }
 
-    public void DrawCard()
-    {        
-        hand = deck.Draw(cardsToDraw);
-        handText.text = "";
+    private void CreateCardSprite(Card card, int handIndex)
+    {
+        string spritePath = GenerateAssetPath(card.GetValue(), card.GetSuitName(), card.GetValueName(), card.IsFaceCard());
 
-        for (int i = 0; i < hand.Count; i++)
+        GameObject cardSprite = new GameObject();
+
+        cardSprite
+            .AddComponent<SpriteRenderer>()
+            .GetComponent<SpriteRenderer>().sprite = AssetDatabase.LoadAssetAtPath<Sprite>(spritePath);
+
+        cardSprite.GetComponent<Transform>().localScale = new Vector3(0.5f, 0.5f);
+
+        float startPosX = -3.0f;
+        float startPosY = -1.0f;
+
+        cardSprite.GetComponent<Transform>().localPosition = (handIndex == 0) ? 
+            new Vector3(startPosX, startPosY) : new Vector3(-startPosX, startPosY);
+
+        sprites.Add(cardSprite);
+    }
+
+    private void DiscardHand()
+    {
+        for (int i = 0; i < sprites.Count; i++)
         {
-            if (hand[i] != null)
-                handText.text += hand[i].GetValueName() + " of " + hand[i].GetSuitName();
-
-            if (i != hand.Count - 1)
-                handText.text += ", ";
+            Destroy(sprites[i]);
         }
+    }
 
-        //Debug.Log("Current card count: " + deck.GetCurrentDeckSize());
-        deckText.text = "Deck: " + deck.GetCurrentDeckSize();
+    private void DisplayWinner()
+    {
+        Card winner = DetermineHigherCard();
+        Debug.Log(winner.GetFullName());
+    }
+
+    private Card DetermineHigherCard()
+    {
+        // assumes two cards to compare
+        if (hand[0].GetValue() > hand[1].GetValue())
+            return hand[0];
+
+        else if (hand[0].GetValue() < hand[1].GetValue())
+            return hand[1];
+
+        else
+        {
+            if (hand[0].GetSuitValue() > hand[1].GetSuitValue())
+                return hand[0];
+
+            else
+                return hand[1];
+        }
+    }
+
+    public void DrawCards()
+    {
+        DiscardHand();
+
+        if (deck.GetCardCount() > 0)
+        {
+            hand = deck.Draw(cardsToDraw);
+            handText.text = "";           
+
+            for (int i = 0; i < hand.Count; i++)
+            {
+                if (hand[i] != null)
+                    handText.text += hand[i].GetValueName() + " of " + hand[i].GetSuitName();
+
+                if (i != hand.Count - 1)
+                    handText.text += ", ";
+
+                // display card
+                CreateCardSprite(hand[i], i);
+            }
+            
+            deckText.text = "Deck: " + deck.GetCardCount();
+
+            // display winner
+            DisplayWinner();
+            
+            // discard old cards
+        }
+        else
+        {
+            Debug.Log("out of cards !");            
+            // refresh deck            
+        }
     }
 }
 
@@ -63,103 +144,6 @@ public class Deck
         deckSize = numCards;
         deck = new List<Card>();
         InitializeDeck(numCards);
-    }
-
-    public int GetOriginalDeckSize()
-    {
-        return deckSize;
-    }
-
-    public int GetCurrentDeckSize()
-    {
-        return deck.Count;
-    }
-
-    public void PrintDeck()
-    {
-        int b = 0;
-        while (b < deck.Count)
-        {
-            Debug.Log(deck[b].GetValue() + " : " + deck[b].GetSuit());
-            b++;
-        }
-
-        //Debug.Log(deck.Count);
-    }
-
-    private int CountCurrentSuit(char suit)
-    {
-        int suitCount = 0;        
-
-        for (int i = 0; i < deck.Count; i++)
-        {
-            if (deck[i].GetSuit().Equals(suit))
-            {
-                suitCount++;
-            }
-        }
-
-        return suitCount;
-    }
-
-    private int SumCurrentDeck()
-    {
-        int b = 0, sum = 0;
-        while (b < deck.Count)
-        {
-            sum += deck[b].GetValue();
-            b++;
-        }
-
-        //Debug.Log("SUM = " + deck.SumDeck());
-
-        return sum;
-    }
-
-    private void InitializeDeck()
-    {
-        int maxCardVal = 13;     
-
-        for (int i = 1; i <= maxCardVal; i++)
-        {
-            Card hearts = new Card(i, 'h');            
-            Card clubs = new Card(i, 'c');
-            Card diamonds = new Card(i, 'd');
-            Card spades = new Card(i, 's');
-
-            deck.Add(hearts);
-            deck.Add(clubs);
-            deck.Add(diamonds);
-            deck.Add(spades);
-        }
-    }
-
-    private void InitializeDeck(int maxVal)
-    {
-        // variant deck build        
-    }
-    
-    public void Shuffle()
-    {
-        int r;
-        for (int i = 0; i < deck.Count; i++)
-        {
-            r = UnityEngine.Random.Range(1, deck.Count);
-            Card temp = deck[i];
-            deck[i] = deck[r];
-            deck[r] = temp;
-        }                   
-    }
-
-    private Card FindCard(int value, char suit)
-    {
-        for (int i = 0; i < deck.Count; i++)
-        {
-            if (deck[i].GetValue() == value && deck[i].GetSuit() == suit)
-                return deck[i];            
-        }
-
-        return null;
     }
 
     public List<Card> Draw(int numToDraw)
@@ -209,6 +193,103 @@ public class Deck
 
         return hand;
     }
+
+    public void Shuffle()
+    {
+        int r;
+        for (int i = 0; i < deck.Count; i++)
+        {
+            r = UnityEngine.Random.Range(1, deck.Count);
+            Card temp = deck[i];
+            deck[i] = deck[r];
+            deck[r] = temp;
+        }
+    }
+
+    public int GetDeckSize()
+    {
+        return deckSize;
+    }
+
+    public int GetCardCount()
+    {
+        return deck.Count;
+    }
+
+    public void PrintDeck()
+    {
+        int b = 0;
+        while (b < deck.Count)
+        {
+            Debug.Log(deck[b].GetValue() + " : " + deck[b].GetSuit());
+            b++;
+        }
+
+        //Debug.Log(deck.Count);
+    }
+
+    private void InitializeDeck()
+    {
+        int maxCardVal = 13;     
+
+        for (int i = 1; i <= maxCardVal; i++)
+        {
+            Card hearts = new Card(i, 'h');            
+            Card clubs = new Card(i, 'c');
+            Card diamonds = new Card(i, 'd');
+            Card spades = new Card(i, 's');
+
+            deck.Add(hearts);
+            deck.Add(clubs);
+            deck.Add(diamonds);
+            deck.Add(spades);
+        }
+    }
+
+    private void InitializeDeck(int maxVal)
+    {
+        // variant deck build        
+    }
+    
+    private Card FindCard(int value, char suit)
+    {
+        for (int i = 0; i < deck.Count; i++)
+        {
+            if (deck[i].GetValue() == value && deck[i].GetSuit() == suit)
+                return deck[i];            
+        }
+
+        return null;
+    }
+
+    private int CountCurrentSuit(char suit)
+    {
+        int suitCount = 0;
+
+        for (int i = 0; i < deck.Count; i++)
+        {
+            if (deck[i].GetSuit().Equals(suit))
+            {
+                suitCount++;
+            }
+        }
+
+        return suitCount;
+    }
+
+    private int SumCurrentDeck()
+    {
+        int b = 0, sum = 0;
+        while (b < deck.Count)
+        {
+            sum += deck[b].GetValue();
+            b++;
+        }
+
+        //Debug.Log("SUM = " + deck.SumDeck());
+
+        return sum;
+    }
 }
 
 public class Card
@@ -218,6 +299,7 @@ public class Card
     private Color color;
     private string valueName;
     private string suitName;
+    private int suitValue;
 
     public Card(int cardValue, char cardSuit)
     {
@@ -225,7 +307,45 @@ public class Card
         suit = cardSuit;
         SetValueName();
         SetSuitName();
+        SetSuitValue();
         SetColor();
+    }
+
+    public bool IsFaceCard()
+    {
+        return (value == 1 || value == 11 || value == 12 || value == 13);
+    }
+
+    public string GetFullName()
+    {
+        return valueName + " of " + suitName;
+    }
+
+    public int GetSuitValue()
+    {
+        return suitValue;
+    }
+
+    private void SetSuitValue()
+    {
+        switch (suit)
+        {
+            case 's':
+                suitValue = 1;
+                break;
+            case 'h':
+                suitValue = 2;
+                break;
+            case 'd':
+                suitValue = 3;
+                break;
+            case 'c':
+                suitValue = 4;
+                break;            
+            default:
+                suitName = null;
+                break;
+        }
     }
 
     public string GetSuitName()
@@ -237,6 +357,9 @@ public class Card
     {
         switch (suit)
         {
+            case 's':
+                suitName = "Spades";
+                break;
             case 'h':
                 suitName = "Hearts";
                 break;
@@ -245,9 +368,6 @@ public class Card
                 break;
             case 'c':
                 suitName = "Clubs";
-                break;
-            case 's':
-                suitName = "Spades";
                 break;
             default:
                 suitName = null;
